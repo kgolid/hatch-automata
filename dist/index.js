7e00eb6a5d0b7aeb370c0b5677273e14030266bf
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2019-12-23 15:02:13 +0100

    update dependencies

--- dist/index.js
+++ dist/index.js
@@ -2706,8 +2706,7 @@
         let px = { h: false, v: false, d: false };
         if (i == 0) px.h = true;
         if (j == 0) px.v = true;
-        if (random_init && (i == 0 || j == 0))
-          px = { h: flip(), v: flip(), d: flip() };
+        if (random_init && (i == 0 || j == 0)) px = { h: flip(), v: flip(), d: flip() };
         row.push(px);
       }
       grid.push(row);
@@ -2717,24 +2716,9 @@
       for (let j = 0; j < grid[i].length; j++) {
         const px = grid[i][j];
         if (i > 0 && j > 0) {
-          px.h = resolve(
-            grid[i][j - 1].h,
-            grid[i - 1][j].v,
-            grid[i - 1][j - 1].d,
-            h_seed
-          );
-          px.v = resolve(
-            grid[i][j - 1].h,
-            grid[i - 1][j].v,
-            grid[i - 1][j - 1].d,
-            v_seed
-          );
-          px.d = resolve(
-            grid[i][j - 1].h,
-            grid[i - 1][j].v,
-            grid[i - 1][j - 1].d,
-            d_seed
-          );
+          px.h = resolve(grid[i][j - 1].h, grid[i - 1][j].v, grid[i - 1][j - 1].d, h_seed);
+          px.v = resolve(grid[i][j - 1].h, grid[i - 1][j].v, grid[i - 1][j - 1].d, v_seed);
+          px.d = resolve(grid[i][j - 1].h, grid[i - 1][j].v, grid[i - 1][j - 1].d, d_seed);
         }
         colorize(j, i, random_init, palette_size);
       }
@@ -2760,18 +2744,17 @@
     if (!cell.d) {
       if (cell.h && !cell.v) cell.tc = cell.lc;
       if (!cell.h && cell.v) cell.lc = cell.tc;
-      if (cell.h && cell.v)
-        cell.tc = cell.lc = new_col(topcol, leftcol, palette_size);
+      if (cell.h && cell.v) cell.tc = cell.lc = new_col(topcol, leftcol, palette_size);
     }
     if (cell.d) {
-      if (cell.h)
-        cell.tc = new_col(topcol, cell.lc ? cell.lc : null, palette_size);
+      if (cell.h) cell.tc = new_col(topcol, cell.lc ? cell.lc : null, palette_size);
       if (cell.v) cell.lc = new_col(leftcol, cell.tc, palette_size);
     }
   }
 
   function new_col(a, b, n) {
     if (b === null) return (a + 1) % n;
+    if (n === 6) return combine6(a, b);
     if (n === 5) return combine5(a, b);
     if (n === 4) return combine4(a, b);
     if (n === 3) return combine3(a, b);
@@ -2781,14 +2764,33 @@
   // ---- UTILS ----
 
   function combine3(x, y) {
-    const arr = [[1, 2, 1], [2, 2, 0], [1, 0, 0]];
+    const arr = [
+      [1, 2, 1],
+      [2, 2, 0],
+      [1, 0, 0]
+    ];
     return arr[y][x];
   }
 
   function combine4(x, y) {
-    const simple = [[1, 2, 1, 1], [2, 0, 0, 0], [1, 0, 0, 0], [1, 0, 0, 0]];
-    const strict = [[1, 2, 3, 1], [2, 3, 0, 2], [3, 0, 3, 0], [1, 2, 0, 1]];
-    const regular = [[1, 3, 1, 2], [3, 2, 0, 0], [3, 0, 3, 1], [2, 2, 1, 0]];
+    const simple = [
+      [1, 2, 1, 1],
+      [2, 0, 0, 0],
+      [1, 0, 0, 0],
+      [1, 0, 0, 0]
+    ];
+    const strict = [
+      [1, 2, 3, 1],
+      [2, 3, 0, 2],
+      [3, 0, 3, 0],
+      [1, 2, 0, 1]
+    ];
+    const regular = [
+      [1, 3, 1, 2],
+      [3, 2, 0, 0],
+      [3, 0, 3, 1],
+      [2, 2, 1, 0]
+    ];
     if (color_combination === 'simple') return simple[y][x];
     if (color_combination === 'strict') return strict[y][x];
     return regular[y][x];
@@ -2803,6 +2805,19 @@
       [2, 0, 3, 1, 2]
     ];
     return arr[y][x];
+  }
+
+  function combine6(x, y) {
+    const arr = [
+      [5, 2, 3, 4, 5, 1],
+      [2, 3, 4, 5, 0, 2],
+      [3, 4, 5, 0, 1, 3],
+      [4, 5, 0, 1, 2, 4],
+      [5, 0, 1, 2, 3, 0],
+      [1, 2, 3, 4, 0, 1]
+    ];
+
+    return arr[x][y];
   }
 
   function resolve(b1, b2, b3, seed) {
